678e31eb7576ab83351391a81a56daae99695083
Ida Darmochwał <ida.mdart@gmail.com>
2022-01-03 23:18:35 +0100

    Edit server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 
 const productsRoutes = require('./routes/products.routes');
 

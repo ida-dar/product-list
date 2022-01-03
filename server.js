bf683040d2d8ba654893459e647f17e49f9d87ba
Ida Darmochwał <ida.mdart@gmail.com>
2022-01-03 23:32:24 +0100

    Edit server.js

--- server.js
+++ server.js
@@ -21,8 +21,9 @@ let dbUrl = '';
 const username = process.env.MONGO_USER;
 const password = process.env.MONGO_PASS;
 
-if(NODE_ENV === 'test') dbUrl = 'mongodb://localhost:27017/ProductListTest';
-else dbUrl = `mongodb+srv://${username}:${password}@cluster0.zlq2v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+if(NODE_ENV === 'production') dbUrl = `mongodb+srv://${username}:${password}@cluster0.zlq2v.mongodb.net/ProductList?retryWrites=true&w=majority`;
+else if(NODE_ENV === 'test') dbUrl = 'mongodb://localhost:27017/ProductListTest';
+else dbUrl = 'mongodb://localhost:27017/ProductList';
 
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
